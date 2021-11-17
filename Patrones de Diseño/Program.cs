3fe92fe7fb6394cb7412520586dc22b84f531d65
Harold García <haroldsalatiel@gmail.com>
2021-11-17 16:54:00 -0600

    Agregar archivos de proyecto.

---
+++ Patrones de Diseño/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones_de_Diseño
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
