4d7b761efa78315f618aab52e49775c385856db1
Fernando ISK <josefernandoco8@gmail.com>
2021-11-30 18:54:33 -0600

    Singleton

---
+++ Patrones de Diseño/Clases/Creacionales/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace patron_singleton
+{
+    public class Singleton
+    {
+        private static Singleton instancia = null;
+        public string mensaje = "";
+        protected Singleton()
+        {
+            mensaje = "instancia cargada";
+        }
+        public static Singleton Instancia
+        {
+            get
+            {
+                if(instancia == null)
+                    instancia = new Singleton();
+                return instancia;
+            }
+        }
+    }
+}
